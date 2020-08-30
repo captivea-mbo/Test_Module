2a541d618c595f3a7efef2d99e41757ed3a4c3f7
Marcos Bolivar <66689110+captivea-mbo@users.noreply.github.com>
2020-08-30 18:53:04 -0400

    Update

---
+++ mb_autoservice/customer.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api,  _
+from odoo.exceptions import ValidationError
+
+
+# How to Create New Models : https://www.youtube.com/watch?v=L6MxDR71_1k&list=PLqRRLx0cl0hoJhjFWkFYowveq2Zn55dhM&index=2
+class AutoservicesCustomer(models.Model):
+    _name = 'autoservices.customer'
+    _description = 'Autoservices Customer'
+
+
+
+
+
+
